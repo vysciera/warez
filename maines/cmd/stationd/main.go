233c08e478c7ca70cb465895d2507b963a490a28
vys <hobbiathan@gmail.com>
2026-09-11 14:31:04 -0600

    .

--- maines/cmd/stationd/main.go
+++ maines/cmd/stationd/main.go
@@ -1,48 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
+// Handling for Ctrl + C | Stale sockets
+
 func main() {
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
 	listener, err := net.Listen("unix", "./station.sock")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer fmt.Println("maines off air")
 	defer listener.Close()
 
-	fmt.Println("maines - online")
+	go func() {
+		<-ctx.Done()
+		listener.Close()
+	}()
+
+	fmt.Println("maines online.")
 	fmt.Println("waiting for listener...")
 
 	conn, err := listener.Accept()
 	if err != nil {
-		log.Println(err)
+		if ctx.Err() == nil {
+			log.Println("accept failed:", err)
+		}
 		return
 	}
 	defer conn.Close()
+
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
 
 	fmt.Println("listener connected")
 
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
-	
+
 	var sequence uint64
 
-	for now := range ticker.C {
-		sequence ++
+	for {
+		select {
+			case <-ctx.Done():
+				return
 
-		_, err := fmt.Fprintf(
-			conn,
-			"%s S01 #%06d 48172 00691 33104\n",
-			now.UTC().Format("15:04:05"),
-			sequence,
-		)
-		if err != nil {
-			log.Println("transmission failed:", err)
-			return
+			case now := <-ticker.C:
+				sequence++
+
+				_, err := fmt.Fprintf(
+					conn,
+					"%s M %07d 48172 00691 33104\n", // One-time Pad impl?
+					now.UTC().Format("15:04:05"),
+					sequence,
+				)
+				if err != nil {
+					if ctx.Err() == nil {
+						log.Println("transmission failed:", err)
+					}
+					return
+				}
 		}
 	}
 }
